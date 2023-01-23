f2bfaf141a8ae2a5ec81afb8ed534e41508a8478
kdany25 <kabadany25@gmail.com>
2023-01-23 21:01:32 +0200

    feat: header in progress

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+	UserIcon,
+	ChevronDownIcon,
+	SearchIcon,
+	AdjustmentsIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
@@ -10,17 +16,33 @@ const HomeScreen = () => {
 		});
 	}, []);
 	return (
-		<SafeAreaView>
+		<SafeAreaView className="bg-white pt-5">
 			<Text className="text-red-500">
-				<View>
+				<View className="flex-row pb-3 items-center mx-4 space-x-2">
 					<Image
 						source={{ uri: "https://links.papareact.com/wru" }}
 						className="h-7 w-7 bg-gray-300 p-4 rounded-full"
 					/>
-					<View>
-						<Text>Deliver Now!</Text>
-						<Text>Current Location</Text>
+					<View className="flex-1">
+						<Text className="font-bold text-gray-400 text-xs">
+							Deliver Now!
+						</Text>
+						<Text className="font-bold text-xl">
+							Current Location
+							<ChevronDownIcon size={20} color="#00CCBB" />
+						</Text>
 					</View>
+					<UserIcon size={35} color="#00CCBB" />
+				</View>
+				<View>
+					<View>
+						<SearchIcon />
+						<TextInput
+							placeholder="Restaurants and cuisines"
+							keyboardType="default"
+						/>
+					</View>
+					<AdjustmentsIcon color="#00CCBB" />
 				</View>
 			</Text>
 		</SafeAreaView>
