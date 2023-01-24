4acbb08977d912a9739cac17168af12dfe2f5123
kdany25 <kabadany25@gmail.com>
2023-01-24 08:28:59 +0200

    feat: home header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import {
 	UserIcon,
 	ChevronDownIcon,
-	SearchIcon,
+	MagnifyingGlassIcon,
 	AdjustmentsIcon,
 } from "react-native-heroicons/outline";
 
@@ -18,7 +18,7 @@ const HomeScreen = () => {
 	return (
 		<SafeAreaView className="bg-white pt-5">
 			<Text className="text-red-500">
-				<View className="flex-row pb-3 items-center mx-4 space-x-2">
+				<View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
 					<Image
 						source={{ uri: "https://links.papareact.com/wru" }}
 						className="h-7 w-7 bg-gray-300 p-4 rounded-full"
@@ -34,15 +34,17 @@ const HomeScreen = () => {
 					</View>
 					<UserIcon size={35} color="#00CCBB" />
 				</View>
-				<View>
-					<View>
-						<SearchIcon />
+
+				<View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+					<View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+						<MagnifyingGlassIcon color="gray" size={20} />
+
 						<TextInput
 							placeholder="Restaurants and cuisines"
 							keyboardType="default"
 						/>
 					</View>
-					<AdjustmentsIcon color="#00CCBB" />
+					{/* <AdjustmentsIcon color="#00CCBB" /> */}
 				</View>
 			</Text>
 		</SafeAreaView>
