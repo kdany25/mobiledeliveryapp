37772da711c91f6ddf3110da74c41e65bd7f255b
kdany25 <kabadany25@gmail.com>
2023-01-24 21:34:16 +0200

    feat: added sanity CMS

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './restaurant'
+import author from './author'
+
+export const schemaTypes = [post, author, category, blockContent]
