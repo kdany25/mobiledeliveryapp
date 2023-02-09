bb47d91b837127da6b4dd8d26cc949dca413d735
kdany25 <kabadany25@gmail.com>
2023-02-09 12:47:39 +0200

    finished restaurant header

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,10 @@ export default function App() {
 			<TailwindProvider>
 				<Stack.Navigator>
 					<Stack.Screen name="Home" component={HomeScreen} />
+					<Stack.Screen
+						name="Restaurant"
+						component={RestaurantScreen}
+					/>
 				</Stack.Navigator>
 			</TailwindProvider>
 		</NavigationContainer>
